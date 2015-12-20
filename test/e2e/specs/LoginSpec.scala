4d4bcb0cf9849349fe4cada92fb3dac985d30e1c
Yuriy Dalekorey <ydalekorei@remit.se>
2015-12-20 18:22:42 +0200

    Changed specification to be run in Chrome and Firefox

--- test/e2e/specs/LoginSpec.scala
+++ test/e2e/specs/LoginSpec.scala
@@ -10,60 +10,66 @@ import play.api.test.FakeApplication
 import security.bcrypt._
 import test.Helpers._
 
-class LoginSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with ChromeFactory {
+class LoginSpec extends PlaySpec with OneServerPerTest with AllBrowsersPerTest {
 
+  override def sharedTests(browser: BrowserInfo): Unit = {
 
-  "Login button on Login page" when {
-    "email is invalid" must {
-      "be disabled" in {
+      "Login button on Login page" when {
+        "email is invalid" must {
+          "be disabled" +browser.name in {
 
-        enterCredentialsOnLoginPage(email = "email@", password = "somepassword")
+            enterCredentialsOnLoginPage(email = "email@", password = "somepassword")
 
-        find(loginPage.loginButton).get must not be 'enabled
-      }
-    }
-
-    "password is empty" must {
-      "be disabled" in {
-
-        enterCredentialsOnLoginPage(email = "correctemail@mail.com", password = "")
-
-        find(loginPage.loginButton).get must not be 'enabled
-      }
-    }
-
-    "email is valid and password is not empty" must {
-      "be enabled" in {
-
-        enterCredentialsOnLoginPage(email = "correctemail@mail.com", password = "some password")
-
-        find(loginPage.loginButton).get must be ('enabled)
-      }
-    }
-  }
-
-  "Home page" must {
-    "be shown" when  {
-      "correct credentials are submitted" in {
-
-        val email = "user@email.com"
-        val password = "userpassword"
-        val account = generate.copy(email = email, password = hashed(password))
-        accountDao.insert(account)
-
-        enterCredentialsOnLoginPage(email = email, password = password)
-
-        click on loginPage.loginButton
-
-        eventually {
-          pageTitle must be ("Dental Clinic - Home")
+            find(loginPage.loginButton).get must not be 'enabled
+          }
         }
 
+        "password is empty" must {
+          "be disabled"+browser.name in {
+
+            enterCredentialsOnLoginPage(email = "correctemail@mail.com", password = "")
+
+            find(loginPage.loginButton).get must not be 'enabled
+          }
+        }
+
+        "email is valid and password is not empty" must {
+          "be enabled"+browser.name in {
+
+            enterCredentialsOnLoginPage(email = "correctemail@mail.com", password = "some password")
+
+            find(loginPage.loginButton).get must be('enabled)
+          }
+        }
       }
-    }
+
+      "Home page" must {
+        "be shown" when {
+          "correct credentials are submitted"+browser.name in {
+
+            val email = "user@email.com"
+            val password = "userpassword"
+            val account = generate.copy(email = email, password = hashed(password))
+            accountDao.insert(account)
+
+            enterCredentialsOnLoginPage(email = email, password = password)
+
+            click on loginPage.loginButton
+
+            eventually {
+              pageTitle must be("Dental Clinic - Home")
+            }
+
+          }
+        }
+      }
+
   }
 
   val loginPage = new LoginPage
+
+
+  override lazy val browsers = Vector(FirefoxInfo(firefoxProfile), ChromeInfo)
 
   override def newAppForTest(testData: TestData): Application =
     FakeApplication(additionalConfiguration = inMemorySlickDatabase())
